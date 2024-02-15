bce0c604d14df536d647f0b7ce3d5098a343de30
ArvoVesi <arvo.vesi@tktk.ee>
2024-02-15 09:47:56 +0200

    add week 3 code

---
+++ DayOfTheWeekIf/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if (weekDay == 0)
+{
+    Console.WriteLine("Praegu on pühapäev");
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("Praegu on esmaspäev");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Praegu on teisipäev");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("Praegu on kolmapäev");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("Praegu on neljapäev");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("Praegu on reede");
+}
+else if (weekDay == 6)
+{
+    Console.WriteLine("Praegu on laupäev");
+}
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek); // 0-6
+
+switch(weekDay) //Väärtus, mida kontrollitakse
+{
+    case 0: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on pühapäev");
+        break;
+    case 1: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on esmaspäev");
+        break;
+    case 2: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on teisipäev");
+        break;
+    case 3: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on kolmapäev");
+        break;
+    case 4: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on neljapäev");
+        break;
+    case 5: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on reede");
+        break;
+    case 6: // Case 0:  == if(weekDay == 0)
+        Console.WriteLine("Praegu on laupäev");
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
+
+Console.WriteLine("Normi päeva!");
